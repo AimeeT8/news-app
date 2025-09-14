8f44f9d6adb978909a515df44b4e8328cc58f974
Aimee Temple <52682021+AimeeT8@users.noreply.github.com>
2025-09-14 15:29:47 -0400

    Initial Commit

---
+++ TakeHomeTest/TakeHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeTestApp.swift
+//  TakeHomeTest
+//
+//  Created by Aimee Temple on 2025-09-14.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
