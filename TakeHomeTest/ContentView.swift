01002748f0979202a9a36bf3ffc6ce2a85bd58c9
Aimee Temple <52682021+AimeeT8@users.noreply.github.com>
2025-09-15 10:15:14 -0400

    Create a viewmodel class to hold the loadArticles function so it's not in the view.

--- TakeHomeTest/ContentView.swift
+++ TakeHomeTest/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var articles = [Article]()
+    
     
     
     var body: some View {
@@ -50,17 +50,7 @@ struct ContentView: View {
         .task(loadArticles)
     }
     
-    func loadArticles() async {
-        do {
-            let url = URL(string: "https://hws.dev/news")!
-            let(data, _) = try await URLSession.shared.data(from: url)
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = .iso8601
-            articles = try decoder.decode([Article].self, from: data)
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
+   
 }
 
 #Preview {
