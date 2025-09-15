4c6d7abd44e3e13b7fad61968709d2fc4332ce36
Aimee Temple <52682021+AimeeT8@users.noreply.github.com>
2025-09-15 10:49:46 -0400

    Create an enum in the view model for loading, loaded and failed. Add those states to the viewmodel.In contentview add a switch statement to display the loading, loaded or error.

--- TakeHomeTest/ContentView.swift
+++ TakeHomeTest/ContentView.swift
@@ -12,9 +12,31 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            List(viewModel.articles, rowContent: ArticleRow.init)
-            .navigationTitle("Take Home Test")
-            .navigationDestination(for: Article.self, destination: ArticleView.init)
+            switch viewModel.loadState {
+            case .failed:
+                ContentUnavailableView {
+                    Text("Load Error")
+                        .font(.headline)
+                } description: {
+                    Text("There was an error loading the articles. Please try again.")
+                } actions: {
+                    Button("Retry") {
+                        Task {
+                            await viewModel.loadArticles()
+                        }
+                    }
+                }
+            default:
+                if viewModel.articles.isEmpty {
+                    ProgressView("Loading...")
+                        .controlSize(.extraLarge)
+                } else {
+                    List(viewModel.articles, rowContent: ArticleRow.init)
+                    .navigationTitle("Take Home Test")
+                    .navigationDestination(for: Article.self, destination: ArticleView.init)
+                }
+            }
+           
         }
         .task(viewModel.loadArticles)
     }
--- TakeHomeTest/ViewModel.swift
+++ TakeHomeTest/ViewModel.swift
@@ -10,19 +10,31 @@ import Foundation
 
 extension ContentView {
     
+    enum LoadState {
+        case loading, loaded, failed
+    }
+    
     @Observable @MainActor
     class ViewModel {
          private(set) var articles = [Article]()
         
+        private(set) var loadState = LoadState.loading
+        private(set) var loadError: (any Error)?
+        
         func loadArticles() async {
+            loadState = .loading
+            
             do {
                 let url = URL(string: "https://hws.dev/news")!
                 let(data, _) = try await URLSession.shared.data(from: url)
                 let decoder = JSONDecoder()
                 decoder.dateDecodingStrategy = .iso8601
                 articles = try decoder.decode([Article].self, from: data)
+                loadState = .loaded
             } catch {
                 print(error.localizedDescription)
+                loadState = .failed
+                loadError = error
             }
         }
         
